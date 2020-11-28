a743bf37025f02c6943dfaf67e56753956286456
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-28 18:31:29 +0530

    Implemented Add Employee(CRUD operation)

--- src/main/java/org/bhavin/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/org/bhavin/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,7 +26,7 @@ public class EmployeeController {
 	
 	// Map data to view employee list
 	@GetMapping("/list")
-	public String viewEmployee(Model theModel) {
+	public String listEmployee(Model theModel) {
 		
 		// get employees from database
 		List<Employee> theEmployees = employeeService.findAll();
@@ -31,7 +34,60 @@ public class EmployeeController {
 		// add data to spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
 	
+	// Map data to view single employee
+	@GetMapping("/list/{employeeId}")
+	public String viewEmployee(@PathVariable int employeeId, Model theModel) {
+		
+		// get employee details
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		//add data to spring model
+		theModel.addAttribute("employees", theEmployee);
+		
+		return "employees/list-employees";
+		
+	}
+	
+	// Map link to show add employee form
+	@GetMapping("/employeeForm")
+	public String employeeForm(Model theModel) {
+		
+		// create model to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+		
+	}
+	
+	// Process form data to save employee
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// redirect to prevent duplicate submission
+		return "redirect:/employees/list";
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/bhavin/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/org/bhavin/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package org.bhavin.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.bhavin.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
 }
